230ae0a4382594d21b1fdb1d41629f749191d88f
Sean Matheson <sean@ctrlplusb.com>
2016-09-01 09:01:56 +0100

    Eslint fixing on dev files.

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,42 +1,41 @@
+/* eslint-disable global-require */
+
 // Whenever wallaby loads the config file (and later uses babel),
 // babel will know that we are in the `test` env.
-process.env.BABEL_ENV = `test`;
-process.env.NODE_ENV = `test`;
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
 process.env.BABEL_DISABLE_CACHE = 1;
 
-module.exports = function (wallaby) {
+module.exports = function wallabyConfiguration(wallaby) {
   return {
     files: [
-      `src/**/*.js`,
-      `test/**/*.js`,
-      `!test/**/*.test.js`,
+      'src/**/*.js',
+      'test/**/*.js',
+      '!test/**/*.test.js',
     ],
 
     tests: [
-      `test/**/*.test.js`
+      'test/**/*.test.js',
     ],
 
     compilers: {
-      '**/*.js': wallaby.compilers.babel({
-        babel: require(`babel-core`),
-        babelrc: true
-      })
+      '**/*.js': wallaby.compilers.babel(),
     },
 
     env: {
-      type: `node`,
-      runner: `node`,
+      type: 'node',
+      runner: 'node',
       params: {
-        env: `NODE_ENV=test` // semi-colon seperated.
-      }
+        env: 'NODE_ENV=test', // semi-colon seperated.
+      },
     },
 
-    testFramework: `mocha`,
+    testFramework: 'mocha',
 
     bootstrap: () => {
-      require(`./test/setup`);
+      require('./test/setup');
     },
 
-    debug: true
+    debug: true,
   };
 };
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,75 +7,75 @@ import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
 
 const env = process.env.NODE_ENV;
 const generateStats = process.env.WEBPACK_STATS || false;
-const libraryName = `react-component-queries`;
+const libraryName = 'react-component-queries';
 const outputFileName = `${libraryName}.js`;
 
 const reactExternal = {
-  root: `React`,
-  commonjs2: `react`,
-  commonjs: `react`,
-  amd: `react`
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react',
 };
 
 const reactSizeMeExternal = {
-  root: `SizeMe`,
-  commonjs2: `react-sizeme`,
-  commonjs: `react-sizeme`,
-  amd: `react-sizeme`
+  root: 'SizeMe',
+  commonjs2: 'react-sizeme',
+  commonjs: 'react-sizeme',
+  amd: 'react-sizeme',
 };
 
 const config = {
-  entry: path.resolve(__dirname, `./src/index.js`),
+  entry: path.resolve(__dirname, './src/index.js'),
   externals: {
     react: reactExternal,
-    'react-sizeme': reactSizeMeExternal
+    'react-sizeme': reactSizeMeExternal,
   },
   module: {
     preLoaders: [
-      { test: /\.js$/, loaders: [`eslint-loader`], exclude: /node_modules/ }
+      { test: /\.js$/, loaders: ['eslint-loader'], exclude: /node_modules/ },
     ],
     loaders: [
       {
         test: /\.js$/,
-        loaders: [`babel-loader`],
-        include: [path.resolve(__dirname, `./src`)],
-        exclude: /node_modules/
-      }
-    ]
+        loaders: ['babel-loader'],
+        include: [path.resolve(__dirname, './src')],
+        exclude: /node_modules/,
+      },
+    ],
   },
   output: {
-    path: path.resolve(__dirname, `./lib`),
+    path: path.resolve(__dirname, './lib'),
     filename: outputFileName,
     library: libraryName,
-    libraryTarget: `umd`,
-    umdNamedDefine: true
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env)
-    })
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
   ],
   eslint: {
     failOnError: true,
-    failOnWarning: true
-  }
+    failOnWarning: true,
+  },
 };
 
-if (env === `production`) {
+if (env === 'production') {
   config.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
       output: {
-        comments: false
+        comments: false,
       },
       compressor: {
         pure_getters: true,
         unsafe: true,
         unsafe_comps: true,
         screw_ie8: true,
-        warnings: false
-      }
+        warnings: false,
+      },
     })
   );
 
@@ -86,9 +86,9 @@ if (env === `production`) {
 
 if (generateStats) {
   config.plugins.push(
-    new WebpackStatsPlugin(`stats.json`, {
+    new WebpackStatsPlugin('stats.json', {
       chunkModules: true,
-      exclude: [/node_modules[\\\/]react/]
+      exclude: [/node_modules[\\\/]react/],
     })
   );
 }
